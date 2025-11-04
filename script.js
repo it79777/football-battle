9e4cb37123fd823db6c731a925e5bd90ea7b8431
it79777 <parsa.zeighami92@gmail.com>
2025-11-04 18:26:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,34 @@
-const startScreen = document.getElementById("start-screen");
-const gameScreen = document.getElementById("game-screen");
-const startBtn = document.getElementById("start-btn");
-const fightBtn = document.getElementById("fight-btn");
-const player1Div = document.getElementById("player1");
-const player2Div = document.getElementById("player2");
-const winnerDiv = document.getElementById("winner");
+// p.win Football Battle ⚡ - Survivor Mode
 
 const players = [
-  "p.win", "مسی", "رونالدو", "رونالدینیو", "پله", "مارادونا", "ام‌باپه", "هالند",
-  "زیدان", "بکهام", "صلاح", "نیمار", "کاکا", "بنزما", "رونی", "راموس", "دی بروینه",
-  "مودریچ", "کروس", "ابراهیموویچ", "ژاوی", "اینیستا", "بوفون", "مالدینی", "پیرلو",
-  "کاسیاس", "نانی", "سانچو", "فودن", "هری کین", "روبرتو کارلوس", "ریوالدو",
-  "توتی", "شوچنکو", "دروگبا", "لمپارد", "جرارد", "کانته", "مارسیال", "دیبالا",
-  "وینیسیوس", "هاوی سیمونز", "تورام", "ژائو فلیکس", "انزو فرناندز", "کولو موآنی",
-  "دل پیرو", "باتیستوتا", "پاول ندود", "پاتریس اورا", "تر اشتگن", "نویِر", "الیسون",
-  "ادرسون", "اوسپینا", "اوبلاک", "ریچارلیسون", "کاسمیرو", "واران", "اوتامندی",
-  "آلابا", "مارکوس یورنته", "سانچز", "آرتور", "پدری", "گاوی", "کولیبالی", "رویس",
-  "هاورتز", "مانه", "ژوتا", "کیتا", "فیرمینو", "مارتینلی", "ساكا", "رایس", "واردی",
-  "تروسار", "گابریل ژسوس", "آلن", "جیمز رودریگز", "والکات", "مارسیو", "سوارز",
-  "کوانی", "گریلیش", "دیاز", "استرلینگ", "مخیتاریان", "دمبله", "گریزمان",
-  "پوگبا", "آسنسیو", "بالاک", "هنری", "برکمپ", "تورس", "ژاوی آلونسو", "رابن"
+  "رونالدینیو", "پله", "مارادونا", "مسی", "رونالدو", "رونی", "بکام", "زیدان",
+  "نیمار", "صلاح", "ام‌باپه", "هالند", "کروس", "لوکا مودریچ", "راموس", "بنزما",
+  "رابرت لواندوفسکی", "وینیسیوس", "دی بروینه", "ریوالدو", "روماریو", "بوفون",
+  "تیری آنری", "ژاوی", "اینیستا", "پویول", "توماس مولر", "سون", "کین", "کاوانی",
+  "رابرت پیرس", "دروگبا", "شیرو", "هری مگوایر 😅", "دیبالا", "توتی", "دل پیرو",
+  "اوزیل", "کاسمیرو", "هالک", "جورج بست", "ریو فردیناند", "کلوزه", "اِتو",
+  "هاوی مارتینز", "باتیستوتا", "کافو", "رائول", "کاکا", "بیرکمپ", "ژرژ وه‌آ"
 ];
 
-let currentPlayers = [];
+let currentChampion = null;
+let opponent = null;
+let remainingPlayers = [];
+const result = document.getElementById("result");
+const startBtn = document.getElementById("start");
+const nextBtn = document.getElementById("next");
 
-startBtn.addEventListener("click", () => {
-  startScreen.classList.add("hidden");
-  gameScreen.classList.remove("hidden");
-  newBattle();
-});
+startBtn.addEventListener("click", startGame);
+nextBtn.addEventListener("click", nextRound);
 
-fightBtn.addEventListener("click", () => {
-  winnerDiv.textContent = "";
-  newBattle();
-});
-
-function newBattle() {
-  const p1 = players[Math.floor(Math.random() * players.length)];
-  const p2 = players[Math.floor(Math.random() * players.length)];
-  if (p1 === p2) return newBattle();
-
-  player1Div.textContent = p1;
-  player2Div.textContent = p2;
-
-  player1Div.onclick = () => selectWinner(p1);
-  player2Div.onclick = () => selectWinner(p2);
+function startGame() {
+  result.innerHTML = "";
+  remainingPlayers = [...players];
+  shuffle(remainingPlayers);
+  currentChampion = remainingPlayers.pop();
+  opponent = remainingPlayers.pop();
+  showMatch();
 }
 
-function selectWinner(winner) {
-  winnerDiv.textContent = `🏆 برنده: ${winner}`;
-  player1Div.onclick = null;
-  player2Div.onclick = null;
-}
+function showMatch() {
+  result.innerHTML = `
+    <h3>⚽
